618b6b12c07997840833e29c0c40b705c1f0041d
Ilyaberezovsky <ilyaberezovskiy2001@mail.ru>
2020-08-21 22:21:55 +0800

    Add restart the game

--- src/main.c
+++ src/main.c
@@ -1,45 +1,52 @@
+#include "logic.h"
+#include "screen.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "screen.h"
-#include "logic.h"
 
 int main()
 {
-	srand(time(NULL));
-	system("clear");
-	display();
-    int count = 100;
+    srand(time(NULL));
+    display();
     char compNum;
-	char buf[32];
-	int inputNum;
+    char buf[32];
+    int inputNum;
     int key = 2;
-    while (count > 0) {
-        printf("Осталось спичек: %d\n", count);
-        printf("Сколько спичек вы хотите взять? ");
-        fgets(buf, 31, stdin);
-        if (sscanf(buf, "%d", &inputNum) != 1) {
-			if(inputNum == EOF) { 
-				exit(0);
-			}
-            printf("Неправильный ввод\n");
-            continue;
-		}
-		if (proverka(inputNum, &count)) {
-			continue;
-		}
-		key = 2;
-        if (count > 0) {
-            compNum = rand() % 10 + 1;
-			check(compNum, &count);
-            key = 1;
+	char choice;
+    while (1) {
+	int count = 100;
+        while (count > 0) {
+            printf("Осталось спичек: %d\n", count);
+            printf("Сколько спичек вы хотите взять? ");
+            fgets(buf, 31, stdin);
+            if (sscanf(buf, "%d", &inputNum) != 1) {
+                if (inputNum == EOF) {
+                    exit(0);
+                }
+                printf("Неправильный ввод\n");
+                continue;
+            }
+            if (proverka(inputNum, &count)) {
+                continue;
+            }
+            key = 2;
+            if (count > 0) {
+                compNum = rand() % 10 + 1;
+                check(compNum, &count);
+                key = 1;
+            }
         }
+		if (key == 1) {
+		printf("  Вы выиграли\n");
+		} else {
+		printf("  Вы проиграли\n");
+    	}
+        printf("Выйти из игры?(n) Продолжить?(y)\n");
+		scanf("%c%*c", &choice);
+        if (choice == 'n' || choice == 'N') 
+			exit(0);
+		if (choice == 'y' || choice == 'Y')
+			display();
     }
-    if (key == 1) {
-        printf("Вы выиграли\n");
-    } else {
-        printf("Вы проиграли\n");
-    }
-
     return 0;
 }
--- src/screen.c
+++ src/screen.c
@@ -1,8 +1,10 @@
 #include "screen.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void display()
 {
+	system("clear");
 	printf("==========================================================\n");
 	printf("==        Добрый день, это игра 100 спичек              ==\n");
 	printf("== Здесь по очереди компьютер и игрок берут 1-10 спичек ==\n");
